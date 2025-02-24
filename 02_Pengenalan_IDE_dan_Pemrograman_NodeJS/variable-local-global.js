db7acc1dbe62227380c0f0f578615eb699fc349f
Alialkhozini <muhammadmahrus2310@gmail.com>
2025-02-24 12:50:16 +0700

    commit pertemuan 2

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVariable = "Muhammad Mahrus Ali" 
+
+function getMyIdentity() {
+    let localVariable = 2311104006
+
+    console.log(`nama saya ${globalVariable} nim ${localVariable}`)
+}
+
+console.log(globalVariable)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/variables.js
@@ -0,0 +1,20 @@
+var name = "Muhammad Mahrus Ali"
+
+console.log(name)
+
+console.log("Nama saya adalah"+name+" dan nim saya adalah " +nim)
+console.log(`nama saya adalah ${name} dan nim saya adalah ${nim}`)
+
+console.log(`nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+
+var name = "ayam"
+
+let fullName = "Muhammad Mahrus Ali"
+
+
+//const kelas
+
+//nim =  2311104006;
